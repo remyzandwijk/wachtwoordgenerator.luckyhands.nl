abe87391498ec64f72c2f8132f6cf9521443c4c2
Remy Zandwijk <me@remyzandwijk.nl>
2023-09-10 17:57:38 +0200

    Use every letter just once.

--- script.js
+++ script.js
@@ -1,54 +1,57 @@
 /* Generate macOS Keychain style passwords */
 const numOfDigits = 1;
-const numOfCapitals = 1;
+const numOfLetters = 1;
 const totalLength = 18;
 
 const passwordTxt = document.getElementById("password");
 const generateBtn = document.getElementById("generate");
 
+
 document.body.onload = () => {
   passwordTxt.value = generatePassword();
 };
+
 
 generateBtn.addEventListener("click", () => {
   passwordTxt.value = generatePassword();
 });
 
-String.prototype.shuffle = function () {
-  var a = this.split(""), n = a.length;
 
-  for(var i = n - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var tmp = a[i];
-      a[i] = a[j];
-      a[j] = tmp;
+const shuffle_array = (input) => {
+  for (let i = input.length -1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [input[i], input[j]] = [input[j], input[i]];
   }
-  return a.join("");
-}
+  return input;
+};
+
 
 const generatePassword = () => {
-  let password = "";
-  var alphabet = "abcdefghijklmnopqrstuvwxyz";
-  var digits = "0123456789";
+  let password = [];
+  var alphabet = shuffle_array(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
+  var digits = shuffle_array(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
 
-  for (let i = 0; i < numOfCapitals; i++) {
-    password += alphabet.charAt(Math.floor(Math.random() * alphabet.length)).toUpperCase();
+ 
+  for (let i = 0; i < numOfLetters; i++) {
+    password.push(alphabet.pop().toUpperCase());
   }
 
   for (let i = 0; i < numOfDigits; i++) {
-    password += digits.charAt(Math.floor(Math.random() * digits.length));
+    password.push(digits.pop());
   }
 
-  for (let i = 0; i < (totalLength - numOfCapitals - numOfDigits); i++) {
-    password += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+  for (let i = 0; i < (totalLength - numOfLetters - numOfDigits); i++) {
+    password.push(alphabet.pop());
   }
 
-  /* Shuffle password string */
-  password = password.shuffle();
+  /* Shuffle password array and convert to string */
+  password_string = shuffle_array(password).join("");
 
   /* Insert dash every six characters */
-  return password.match(/.{1,6}/g).join('-');
+  return password_string.match(/.{1,6}/g).join('-');
 };
+
 
 const copyBtn = document.getElementById("copy");
 copyBtn.addEventListener("click", () => {
