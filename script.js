207de5c430d7c92bfb56f7312d91d907417ae1ea
Remy Zandwijk <me@remyzandwijk.nl>
2026-04-19 16:45:13 +0200

    Simplify code and make use of better crypto

--- script.js
+++ script.js
@@ -1,59 +1,52 @@
-/* Generate macOS Keychain style passwords */
-const numOfDigits = 1;
-const numOfLetters = 1;
-const totalLength = 18;
+// Generate macOS Passwords style passwords
+
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const digits = "0123456789";
+
+const getRandomInt = (max) => {
+  const arr = new Uint32Array(1);
+  crypto.getRandomValues(arr);
+  return arr[0] % max;
+}
+
+const shuffle = (arr) => {
+  for (let i = arr.length -1; i > 0; i--) {
+    const j = getRandomInt(i + 1);
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+};
+
+const generatePassword = (length = 18) => {
+  let password = [];
+
+  // Required characters
+  password.push(letters[getRandomInt(letters.length)].toUpperCase());
+  password.push(digits[getRandomInt(digits.length)]);
+
+  for (let i = 0; i < (length - 2); i++) {
+    password.push(letters[getRandomInt(letters.length)]);
+  }
+
+  // Shuffle password
+  password = shuffle(password);
+
+  // Insert dash every six characters
+  return password.join("").match(/.{1,6}/g).join('-');
+};
 
 const passwordTxt = document.getElementById("password");
 const generateBtn = document.getElementById("generate");
+const copyBtn = document.getElementById("copy");
 
-
-document.body.onload = () => {
+window.addEventListener("load", () => {
   passwordTxt.value = generatePassword();
-};
-
+});
 
 generateBtn.addEventListener("click", () => {
   passwordTxt.value = generatePassword();
 });
 
-
-const shuffle_array = (input) => {
-  for (let i = input.length -1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [input[i], input[j]] = [input[j], input[i]];
-  }
-  return input;
-};
-
-
-const generatePassword = () => {
-  let password = [];
-  var alphabet = shuffle_array(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-                                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
-  var digits = shuffle_array(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
-
- 
-  for (let i = 0; i < numOfLetters; i++) {
-    password.push(alphabet.pop().toUpperCase());
-  }
-
-  for (let i = 0; i < numOfDigits; i++) {
-    password.push(digits.pop());
-  }
-
-  for (let i = 0; i < (totalLength - numOfLetters - numOfDigits); i++) {
-    password.push(alphabet.pop());
-  }
-
-  /* Shuffle password array and convert to string */
-  password_string = shuffle_array(password).join("");
-
-  /* Insert dash every six characters */
-  return password_string.match(/.{1,6}/g).join('-');
-};
-
-
-const copyBtn = document.getElementById("copy");
 copyBtn.addEventListener("click", () => {
   navigator.clipboard.writeText(passwordTxt.value);
   passwordTxt.style.backgroundColor = 'green';
